e179e801a0bec2200ca9be5210050cc725b876c8
huxiaolong <1hxl@163.com>
2018-02-09 17:43:40 +0800

    commit

---
+++ src/main/java/com/chenxi/CxManagementApplication.java
@@ -0,0 +1,12 @@
+package com.chenxi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CxManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CxManagementApplication.class, args);
+	}
+}
